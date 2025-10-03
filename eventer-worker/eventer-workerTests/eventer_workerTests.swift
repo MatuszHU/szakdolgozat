d6c34ea59636f4b9b350f2d5a7712cf6f3e261c8
Majoros Máté <matusz@mailbox.unideb.hu>
2025-10-03 21:55:17 +0200

    Initializing the 2 mobile apps (codename: eventer -guest, -worker)

---
+++ eventer-worker/eventer-workerTests/eventer_workerTests.swift
@@ -0,0 +1,17 @@
+//
+//  eventer_workerTests.swift
+//  eventer-workerTests
+//
+//  Created by Majoros Máté on 2025. 10. 03..
+//
+
+import Testing
+@testable import eventer_worker
+
+struct eventer_workerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
