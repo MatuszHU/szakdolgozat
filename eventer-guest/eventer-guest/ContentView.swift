36c7681dfa7f046a0a4d0f9b36e73f3f91088053
Majoros Máté <matusz@mailbox.unideb.hu>
2025-10-04 18:36:37 +0200

    Guest app - Adding welcome screen view

--- eventer-guest/eventer-guest/ContentView.swift
+++ eventer-guest/eventer-guest/ContentView.swift
@@ -13,46 +13,26 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        
+        VStack{
+            Spacer()
+            Text("Eventer Guest")
+                .font(.largeTitle)
+                .padding()
+            Text("Köszöntelek az Eventer Béta Vendég appjában")
+            Text("A tovább lépéshez kattints a nyílra!")
+            Spacer()
+            Button("Sign In", systemImage: "arrow.forward",action: {})
+                .labelStyle(.iconOnly).font(.largeTitle)
+                .padding(32)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(50)
+                .glassEffect()
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
