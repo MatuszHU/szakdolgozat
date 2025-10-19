b315e8eae992ca73ba77256bd72d65364adaa667
Majoros Máté <matusz@mailbox.unideb.hu>
2025-10-19 21:21:31 +0200

    deleted code

---
+++ eventer-guest/eventer-guest/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  eventer-guest
+//
+//  Created by Majoros Máté on 2025. 10. 18..
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ eventer-guest/eventer-guestTests/eventer_guestTests.swift
@@ -0,0 +1,17 @@
+//
+//  eventer_guestTests.swift
+//  eventer-guestTests
+//
+//  Created by Majoros Máté on 2025. 10. 18..
+//
+
+import Testing
+@testable import eventer_guest
+
+struct eventer_guestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
