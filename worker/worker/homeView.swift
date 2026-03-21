c9f55d6f7436c5dea36384d65579de6b5174d498
Majoros Máté <matusz@mailbox.unideb.hu>
2026-03-21 16:18:31 +0100

    Rollback to start a new webbased client

---
+++ worker/worker/homeView.swift
@@ -0,0 +1,18 @@
+//
+//  homeView.swift
+//  worker
+//
+//  Created by Majoros Máté on 2026. 03. 09..
+//
+
+import SwiftUI
+
+struct homeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    homeView()
+}
---
+++ worker/worker/welcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  welcomeView.swift
+//  worker
+//
+//  Created by Majoros Máté on 2026. 03. 08..
+//
+
+import SwiftUI
+
+struct welcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    welcomeView()
+}
